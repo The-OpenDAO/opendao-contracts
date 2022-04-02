ea4a390da6a29db1fc810e6a396a301ee968e1f3
aliceBob <aliceBob@gmail.com>
2022-04-02 17:45:25 +0800

    refactor

--- contracts/OpenDAOWLDistribute.sol
+++ contracts/OpenDAOWLDistribute.sol
@@ -9,50 +9,55 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 contract OpenDAOWLDistribute is Ownable {
     using SafeERC20 for IERC20;
 
-    event RequestWL(address indexed requester, uint count, uint projectID);
+    event RequestWL(address requester, uint projectID, uint256 burned, uint256 treasury);
+    event ProjectAdded(uint256 projectID, string name);
 
     IERC20 public immutable sosToken;
     address public immutable treasury;
-    uint256 nextProjectID = 1;
+    uint256 public nextProjectID = 1;
 
     struct Project {
+        uint40 price;
         uint16 totalSupply;
         uint16 totalBought;
-        uint8 maxPerWallet;
-        bool allowContract;
-        uint40 price;
-        uint32 startBlock;
+        uint48 startTime;
         bool isEnabled;
+        uint48 endTime;
     }
 
     mapping(uint256 => Project) public projects; //map from project id to project object
-    mapping(uint256 => string) public projectNames; //lookup project name by project id
-    mapping(uint256 => mapping (address => uint8)) public ownedWLs; //how many whitelist each address bought
-    mapping(string => uint256) public projectIds; //lookup project id by project name
+    mapping(uint256 => mapping (address => uint256)) public ownedWLs; //how many whitelist each address bought
+
+    // mapping(string => uint256) public projectIds; //lookup project id by project name
+    // mapping(uint256 => string) public projectNames; //lookup project name by project id
 
     constructor(IERC20 _sosToken, address _treasury) {
         sosToken = _sosToken;
         treasury = _treasury;
     }
 
-    function addProject(uint16 totalSupply, uint40 price, uint8 maxPerWallet, string memory projectName) external onlyOwner {
-        require(projectIds[projectName] == 0, "DuplicateProjectName");
+    function addProject(string calldata projectName, Project memory _project) external onlyOwner {
+        // require(projectIds[projectName] == 0, "DuplicateProjectName");
 
-        projects[nextProjectID].totalSupply = totalSupply;
-        projects[nextProjectID].totalBought = 0;
-        projects[nextProjectID].maxPerWallet = maxPerWallet;
-        projects[nextProjectID].allowContract = false;
-        projects[nextProjectID].price = price;
-        projects[nextProjectID].startBlock = uint32(block.number);
-        projects[nextProjectID].isEnabled = false;
-        projectNames[nextProjectID] = projectName;
-        projectIds[projectName] = nextProjectID;
+        require(_project.totalSupply > 0, "");
+        require(_project.endTime >= block.timestamp, "");
+        
+        _project.totalBought = 0;
+        projects[nextProjectID] = _project;
+
+        // projectNames[nextProjectID] = projectName;
+        // projectIds[projectName] = nextProjectID;
+        emit ProjectAdded(nextProjectID, projectName);
         ++nextProjectID;
     }
 
-    function toggleAllowContract(uint256 projectID) external onlyOwner {
-        require(projects[projectID].totalSupply > 0, "InvalidProject");
-        projects[projectID].allowContract = !projects[projectID].allowContract;
+    function setProject(Project calldata _project, uint256 projectID) external onlyOwner {
+        require(_project.totalSupply > 0, "");
+        require(_project.endTime >= block.timestamp, "");
+        Project storage p = projects[projectID];
+        p.endTime = _project.endTime;
+        //....
+        p.isEnabled = _project.isEnabled;
     }
 
     function toggleEnable(uint256 projectID) external onlyOwner {
@@ -60,36 +65,34 @@ contract OpenDAOWLDistribute is Ownable {
         projects[projectID].isEnabled = !projects[projectID].isEnabled;
     }
 
-    function getProjectID(string memory projectName) external view returns(uint256 pid) {
-        return projectIds[projectName];
-    }
+    // function getProjectID(string memory projectName) external view returns(uint256 pid) {
+    //     return projectIds[projectName];
+    // }
 
-    function requestWL(uint256 projectID, uint8 count) external {
+    function requestWL(uint256 projectID) external {
         require(projects[projectID].totalSupply > 0, "InvalidProject");
-        Project storage p = projects[projectID];
+        Project memory p = projects[projectID];
 
         require(p.isEnabled, "NotEnabled");
-        if (!p.allowContract) {
-            require(tx.origin == msg.sender, "NotAllowContract");
-        }
+        require(tx.origin == msg.sender, "NotAllowContract");
+        require(p.endTime >= block.timestamp, "");
+        require(p.startTime <= block.timestamp, "");
+        require(ownedWLs[projectID][msg.sender] == 0, "");
 
         uint256 price = uint256(p.price) * 1 ether;
-        uint256 amount = price * uint256(count);
 
-        sosToken.safeTransferFrom(msg.sender, address(this), amount * 90 / 100);
-        sosToken.safeTransferFrom(msg.sender, treasury, amount - amount * 90 / 100);
+        uint256 burned = price * 90 / 100;
+        sosToken.safeTransferFrom(msg.sender, address(1), burned);
+        sosToken.safeTransferFrom(msg.sender, treasury, price - burned);
 
-        if (p.maxPerWallet!=0) {
-            require(ownedWLs[projectID][msg.sender] + count <= p.maxPerWallet, "ExeceedMaxPerWallet");
-        }
-        require(p.totalBought + count <= p.totalSupply, "ExeceedMaxSupply");
+        require(p.totalBought + 1 <= p.totalSupply, "ExeceedMaxSupply");
 
-        ownedWLs[projectID][msg.sender] += count;
-        p.totalBought += count;
+        ownedWLs[projectID][msg.sender] = 1;
+        p.totalBought += 1;
 
-        emit RequestWL(msg.sender, count, projectID);
+        projects[projectID] = p;
+        emit RequestWL(msg.sender, projectID, burned, price - burned);
     }
-
 }
 
 
